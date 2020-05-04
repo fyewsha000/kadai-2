0f4a4863512c3872b3f7ce608ace53a20d7eb127
佐々木 優葉 <yuha000@MacBook-Pro.local>
2020-05-04 14:38:49 +0900

    [Update]編集画面&詳細画面実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,11 @@ class BooksController < ApplicationController
 
   def index
   	@book = Book.new
+  	@books = Book.all
   end
 
   def show
+  	@book = Book.find(params[:id])
 
   end
 
@@ -14,11 +16,28 @@ class BooksController < ApplicationController
 
   def create
   	book = Book.new(book_params)
-  	book.save
-  	redirect_to books_path
-  end
+  	if book.save
+  	   flash[:notice]="Book was successfully created!"
+  	  redirect_to book_path(book.id)
+  	end
+   end
 
   def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	if book.update(book_params)
+  		flash[:notice]="Book was successfully updated!"
+  	  redirect_to book_path(book)
+    end
+  end
+
+  def destroy
+  	@book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to book_path(book.id)
   end
 
   private
